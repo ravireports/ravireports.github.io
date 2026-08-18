fc573ccd337e76b9adce799d464f3b8af8252b8c
ravireports <hafizaaishasadeeqa@gmail.com>
2026-08-18 23:39:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const SUPABASE_URL = "https://sajhbelkmserodeanrer.supabase.co";
+const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
 function scrollToContact() {
     document.getElementById("contact").scrollIntoView({
         behavior: "smooth"
@@ -6,7 +14,7 @@ function scrollToContact() {
 
 document
     .getElementById("contactForm")
-    .addEventListener("submit", function (event) {
+    .addEventListener("submit", async function (event) {
 
         event.preventDefault();
 
@@ -18,6 +26,25 @@ document
 
         if (!name || !phone || !message) {
             formMessage.textContent = "Please fill in all fields.";
+            return;
+        }
+
+        formMessage.textContent = "Sending...";
+
+        const { error } = await supabaseClient
+            .from("contact_messages")
+            .insert([
+                {
+                    name: name,
+                    phone: phone,
+                    message: message
+                }
+            ]);
+
+        if (error) {
+            console.error(error);
+            formMessage.textContent =
+                "Something went wrong. Please try again.";
             return;
         }
 
